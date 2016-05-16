050220a2c7b751799f72ea8940e46dfe3f77214e
WindQAQ <b03902042@ntu.edu.tw>
2016-05-16 11:33:31 +0800

    modify trivial things

--- hw3/mapping.sh
+++ hw3/mapping.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-function parse()
-{
-	LANG=zh_TW.big5
-	LC_ALL=zh_TW.big5
+LANG=zh_TW.big5
+LC_ALL=zh_TW.big5
 
-	gawk -F '[ /]' '{
-						for(i=1; i<=NF; i++){
-							if(!x[$1,substr($i,1,1)]++){
-								len[substr($i,1,1)]++; 
-								y[substr($i,1,1)][len[substr($i,1,1)]]=$1 
-							}
+gawk -F '[ /]' '{
+					for(i=1; i<=NF; i++){
+						if(!x[$1,substr($i,1,1)]++){
+							len[substr($i,1,1)]++; 
+							y[substr($i,1,1)][len[substr($i,1,1)]]=$1 
 						}
-					} 
-					END{
-						for(i in y){ 
-							printf "%s\t",i; 
-							for(j in y[i])
-								printf "%s ", y[i][j]; 
-							print ""
-						}
-					}' $1
-}
-
-parse $1 >$2
+					}
+				} 
+				END{
+					for(i in y){ 
+						printf "%s\t",i; 
+						for(j in y[i])
+							printf "%s ", y[i][j]; 
+						print ""
+					}
+				}' $1 > $2
